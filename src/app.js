80f982ba49f6246be05c13d4acd35865e1e2d469
Matheus dos Santos Paixão <137527894+MatheusSantos360@users.noreply.github.com>
2024-08-04 18:24:03 +0000

    app.engine was changed to "html".

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 const routes = require('./routes');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true,}));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "/views"));
 app.use(routes);
-
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
